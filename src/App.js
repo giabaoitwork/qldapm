7269b8f09001d9c08c1728ad11ec4bad86ac4d44
dophunguyenchuong17072002 <20130212@st.hcmuaf.edu.vn>
2023-12-02 17:17:54 +0700

    giao dien home page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import GamePage from './pages/GamePage';
 import UserInfoPage from './pages/UserInfoPage';
 import RankPage from './pages/RankPage';
 import AllReviewPage from './pages/AllReviewPage';
+import HomePage from './pages/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div className="w-screen h-screen relative">
             <img className="object-cover w-full h-full absolute -z-[1]" src={assets.images.background} alt="" />
-            <AllReviewPage />
+            <HomePage/>
         </div>
     );
 }
