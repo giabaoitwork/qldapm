9a9c337589db454042dff3ce7477f4575083312b
HoThanhHoaiAn <124787635+hoaian1232812@users.noreply.github.com>
2023-12-02 16:14:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import GamePage from './pages/GamePage';
 import UserInfoPage from './pages/UserInfoPage';
 import RankPage from './pages/RankPage';
 import AllReviewPage from './pages/AllReviewPage';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
     return (
-        <div className="w-screen h-screen relative">
-            <img className="object-cover w-full h-full absolute -z-[1]" src={assets.images.background} alt="" />
-            <AllReviewPage />
-        </div>
+        <Router>
+            <div className="w-screen h-screen relative">
+                <img className="object-cover w-full h-full absolute -z-[1]" src={assets.images.background} alt="" />
+                <Routes>
+                    <Route path="/game" element={<GamePage />} />
+                    <Route path="/userinfo" element={<UserInfoPage />} />
+                    <Route path="/rank" element={<RankPage />} />
+                    <Route path="/review" element={<ReviewPage />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
